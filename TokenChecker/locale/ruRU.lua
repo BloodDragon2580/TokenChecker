7627a3590d288c50437c580082f10db90e2b9669
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2025-05-24 04:25:18 +0500

    Update ruRU.lua

--- TokenChecker/locale/ruRU.lua
+++ TokenChecker/locale/ruRU.lua
@@ -1,8 +1,8 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("TokenChecker", "ruRU")
 if not L then return end
 
-L["inactiv"] = "|cFFFFFFFFЦена токена:|r |cFF888888n/a|r"
-L["active"] = "|cFFFFFFFFЦена токена:|r |cFF00FF00%s|r"
-L["Price Watch"] = "|cFFFFFFFFНаблюдение за ценой токена|r"
-L["5 minutes"] = "|cFFFFFFFFЦена обновляется каждые 5 минут|r"
-L["fast refresh"] = "|cFFFFFFFFНажмите, чтобы быстро обновить текущую рыночную цену|r"
+L["inactiv"] = "|cFFFFFFFFСтоимость жетона:|r |cFF888888n/a|r"
+L["active"] = "|cFFFFFFFFСтоимость жетона:|r |cFF00FF00%s|r"
+L["Price Watch"] = "|cFFFFFFFFНаблюдение за стоимостью жетона|r"
+L["5 minutes"] = "|cFFFFFFFFСтоимость обновляется каждые 5 минут|r"
+L["fast refresh"] = "|cFFFFFFFFНажмите, чтобы быстро обновить текущую рыночную стоимость|r"
