cec02151934f71caa7dba546d703ab6c30731734
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2025-05-24 04:21:47 +0500

    Create ruRU.lua

---
+++ TokenChecker/locale/ruRU.lua
@@ -0,0 +1,8 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("TokenChecker", "ruRU")
+if not L then return end
+
+L["inactiv"] = "|cFFFFFFFFЦена токена:|r |cFF888888n/a|r"
+L["active"] = "|cFFFFFFFFЦена токена:|r |cFF00FF00%s|r"
+L["Price Watch"] = "|cFFFFFFFFНаблюдение за ценой токена|r"
+L["5 minutes"] = "|cFFFFFFFFЦена обновляется каждые 5 минут|r"
+L["fast refresh"] = "|cFFFFFFFFНажмите, чтобы быстро обновить текущую рыночную цену|r"
